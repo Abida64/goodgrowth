78840b8c27ac5d11860f8b0ae8ebea7e57babea3
Abida Sultana <abida.sultana9412@gmail.com>
2024-06-16 17:03:43 +0100

    a b testing code implemented

--- main.js
+++ main.js
@@ -1,52 +1,37 @@
 const urlTemp =
 	"https://europe-west1-amigo-actions.cloudfunctions.net/recruitment-mock-weather-endpoint/forecast?appid=a2ef86c41a&lat=27.987850&lon=86.925026";
+const nationalTrust = new URLSearchParams(window.location.search);
+const testParams = nationalTrust.has("test");
 //check url of current site has parameter present, to differentiate between A/B
-// if url contains a paramter e.g. test A, run the script else, exit out the func.
-async function fetchTemperature() {
-	const response = await fetch(urlTemp, {
-		headers: {
-			Accept: "application/json",
-		},
-	});
-	//if failed
-	if (!response.ok) {
-		console.error(response.status);
-		console.error(await response.text());
-		return;
-	}
-	const data = await response.json();
-	//pull temp from json object
-	const temp = data.list[0].main.temp;
-	//convert temp to 1 decimal place
-	const tempConverted = temp.toFixed(1);
-	//temp display
-	console.log(tempConverted + "°C");
+// if url contains a paramter e.g. test, run the script else, dont run the script
+if (testParams) {
+	async function fetchTemperature() {
+		const response = await fetch(urlTemp, {
+			headers: {
+				Accept: "application/json",
+			},
+		});
+		//if failed
+		if (!response.ok) {
+			console.error(response.status);
+			console.error(await response.text());
+			return;
+		}
+		const data = await response.json();
+		//pull temp from json object
+		const temp = data.list[0].main.temp;
+		//convert temp to 1 decimal place
+		const tempConverted = temp.toFixed(1);
+		//temp display
+		console.log(tempConverted + "°C");
 
-	//2. inject into the DOM next to region
-	let additionalText = document.createTextNode(", " + tempConverted + "°C");
-	let className = document.querySelector(
-		".Pillstyle__StyledPill-sc-1tc0qet-0.jpeAyR.PlaceSummarystyle__StyledPill-sc-uf3onk-1.htYDvg"
-	);
-	className.appendChild(additionalText);
+		//2. inject into the DOM next to region
+		let additionalText = document.createTextNode(", " + tempConverted + "°C");
+		let className = document.querySelector(
+			".Pillstyle__StyledPill-sc-1tc0qet-0.jpeAyR.PlaceSummarystyle__StyledPill-sc-uf3onk-1.htYDvg"
+		);
+		className.appendChild(additionalText);
+	}
 }
 //call function
 fetchTemperature();
-
-// function requestXMLHttpRequest() {
-// 	var xhr = new XMLHttpRequest();
-// 	xhr.responseType = "json"; //xhr response will populate as json
-// 	xhr.open("GET", urlTemp);
-// 	xhr.send();
-// 	xhr.onload = function () {
-// 		if (xhr.status != 200) {
-// 			//2
-// 			console.log("XMLHttpRequest error: " + xhr.status);
-// 			return;
-// 		}
-// 		processWeatherData(xhr.response); //3
-// 	};
-// 	xhr.onerror = function () {
-// 		//4
-// 		console.log("XMLHttpRequest Request failed");
-// 	};
-// }
